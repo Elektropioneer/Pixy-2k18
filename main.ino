77819cb8d5a0bd0c3881d573e34f0410cbd13913
DJ <33149171+PlaguedByWeirdness@users.noreply.github.com>
2017-11-16 15:55:52 +0100

    Fixed niz-name issue

--- main.ino
+++ main.ino
@@ -55,7 +55,7 @@ void setup()
   int second_block_signature;
   int third_block_signature;
   
-  int niz[3] = {0,0,0};               //Array in which we sort the signatures according to x-axis
+  int signature_array[3] = {0,0,0};               //Array in which we sort the signatures according to x-axis
 
   pixy.init();                        //Pixy define function
 }
@@ -65,7 +65,7 @@ void loop()
 { 
   uint16_t blocks;   
   
-   niz[3] = {0,0,0}; 
+   signature_array[3] = {0,0,0}; 
 
    blocks = pixy.getBlocks();         //Getting all the information about the blocks
    first_block_x = pixy.blocks[0].x;              //Variables for easier code understanding
@@ -79,49 +79,49 @@ void loop()
    if (blocks == 3){                  //Only for three colors without black
 
    if (first_block_x > second_block_x && first_block_x > third_block_x) {        //Sorting of an array by x-axis (if first block is larger than the rest) 
-    niz[0] = first_block_signature;
+    signature_array[0] = first_block_signature;
     if (second_block_x > third_block_x){
-      niz[1] = second_block_signature;
-      niz[2] = third_block_signature;
+      signature_array[1] = second_block_signature;
+      signature_array[2] = third_block_signature;
     }else{
-      niz[2] = second_block_signature;
-      niz[1] = third_block_signature;
+      signature_array[2] = second_block_signature;
+      signature_array[1] = third_block_signature;
     }
   }
 
   if (second_block_x > first_block_x && second_block_x > third_block_x) {         //Sorting of an array by x-axis (if second block is larger than the rest)
-    niz[0] = second_block_signature;
+    signature_array[0] = second_block_signature;
     if (first_block_x > third_block_x){
-      niz[1] = first_block_signature;
-      niz[2] = third_block_signature;
+      signature_array[1] = first_block_signature;
+      signature_array[2] = third_block_signature;
     }else{
-      niz[2] = first_block_signature;
-      niz[1] = third_block_signature;
+      signature_array[2] = first_block_signature;
+      signature_array[1] = third_block_signature;
     }
   }
 
   if (third_block_x > second_block_x && third_block_x > first_block_x) {         //Sorting of an array by x-axis (if third block is larger than the rest)
-    niz[0] = third_block_signature;
+    signature_array[0] = third_block_signature;
     if (second_block_x > first_block_x){
-      niz[1] = second_block_signature;
-      niz[2] = first_block_signature;
+      signature_array[1] = second_block_signature;
+      signature_array[2] = first_block_signature;
     }else{
-      niz[2] = second_block_signature;
-      niz[1] = first_block_signature;
+      signature_array[2] = second_block_signature;
+      signature_array[1] = first_block_signature;
     }
   }
            //Sending a signal according to combinations provided (3 blocks)
-  if(niz[2]== GREEN && niz[1] == YELLOW && niz[0] == BLUE)      //Combination num.2        
+  if(signature_array[2]== GREEN && signature_array[1] == YELLOW && signature_array[0] == BLUE)      //Combination num.2        
     digitalWrite(PIND3, HIGH);
-  if(niz[2]== BLUE && niz[1] == GREEN && niz[0] == ORANGE){     //Combination num.5
+  if(signature_array[2]== BLUE && signature_array[1] == GREEN && signature_array[0] == ORANGE){     //Combination num.5
     digitalWrite(PIND2, HIGH);
     digitalWrite(PIND4, HIGH);
   }
-  if(niz[2]== GREEN && niz[1] == ORANGE && niz[0] == YELLOW){   //Combination num.6
+  if(signature_array[2]== GREEN && signature_array[1] == ORANGE && signature_array[0] == YELLOW){   //Combination num.6
     digitalWrite(PIND3, HIGH);
     digitalWrite(PIND4, HIGH);
   }
-  if(niz[2]== ORANGE && niz[1] == BLUE && niz[0] == YELLOW){    //Combination num.10
+  if(signature_array[2]== ORANGE && signature_array[1] == BLUE && signature_array[0] == YELLOW){    //Combination num.10
     digitalWrite(PIND5, HIGH);
     digitalWrite(PIND3, HIGH);
   }
@@ -129,35 +129,35 @@ void loop()
  
  if (blocks==2){                        //For two colors, ignoring black, doesn't matter if it's there or nah
    if (first_block_x > second_block_x){                   //Sorting of an array by x-axis (if first block is larger)
-     niz[0] = first_block_signature;
-     niz[1] = second_block_signature;
+     signature_array[0] = first_block_signature;
+     signature_array[1] = second_block_signature;
    }
    if (second_block_x > first_block_x){                  //Sorting of an array by x-axis (if second block is larger)
-     niz[1] = first_block_signature;
-     niz[0] = second_block_signature;
+     signature_array[1] = first_block_signature;
+     signature_array[0] = second_block_signature;
    }
    
    
           //Sending a signal according to combinations provided (2 blocks)
-  if(niz[1] == ORANGE && niz[0] == GREEN){                          //Combination num.1
-    digitalWrite(PIND2, HIGH);
+  if(signature_array[1] == ORANGE && signature_array[0] == GREEN){                         //Combination num.1
+    digitalWrite(PIND2, HIGH); 
   }
-  if(niz[1] == YELLOW && niz[0] == BLUE){                        //Combination num.3
-    digitalWrite(PIND2, HIGH);
+  if(signature_array[1] == YELLOW && signature_array[0] == BLUE){                         //Combination num.3
+    digitalWrite(PIND2, HIGH); 
     digitalWrite(PIND3, HIGH);
   }
-  if(niz[1] == BLUE && niz[0] == ORANGE){                           //Combination num.4  
+  if(signature_array[1] == BLUE && signature_array[0] == ORANGE){                         //Combination num.4  
     digitalWrite(PIND4, HIGH);
   }
-  if(niz[1] == YELLOW && niz[0] == GREEN){                       //Combination num.7  
+  if(signature_array[1] == YELLOW && signature_array[0] == GREEN){                        //Combination num.7  
     digitalWrite(PIND2, HIGH);
     digitalWrite(PIND3, HIGH);
     digitalWrite(PIND4, HIGH);
   }
-  if(niz[1] == BLUE && niz[0] == GREEN){                         //Combination num.8
+  if(signature_array[1] == BLUE && signature_array[0] == GREEN){                          //Combination num.8
     digitalWrite(PIND5, HIGH);
-  }
-  if(niz[1] == YELLOW && niz[0] == ORANGE){                      //Combination num.9
+  }  
+  if(signature_array[1] == YELLOW && signature_array[0] == ORANGE){                       //Combination num.9
     digitalWrite(PIND2, HIGH);
     digitalWrite(PIND5, HIGH);
   }
